ecfc77d336219248a1f72216542ef9edf560b7f9
virendra1485 <virendrasinghrawat1485@gmail.com>
2024-07-04 13:33:56 +0530

    Update requirements

--- extensions.py
+++ extensions.py
@@ -6,12 +6,10 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
-from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate(compare_type=True)
 cors = CORS()
 api = Api()
 ma = Marshmallow()
-login_manager = LoginManager()
 jwt = JWTManager()
